f717c2bea43edfe95249c02d2cc0cea209940436
William Sawyer <wmsawyer2609@gmail.com>
2022-10-31 10:07:17 +1000

    Allowed JS access to assets

---
+++ data/icons.js
@@ -0,0 +1,19 @@
+// OgCiSum icons
+import appIcon from "../assets/icons/app-icon.png";
+import appLaunch from "../assets/icons/app-launch.png";
+import logoDarkPurple from "../assets/icons/logo-dark-purple.png";
+import logoWhite from "../assets/icons/logo-white.png";
+
+// map icons
+import iconPinDarkPurple from "../assets/icons/icon-pin-dark-purple.png";
+import iconPinLightPurple from "../assets/icons/icon-pin-light-purple.png";
+import iconPinWhite from "../assets/icons/icon-pin-white.png";
+import tabMapDarkPurple from "../assets/icons/tab-map-dark-purple.png";
+import tabMapWhite from "../assets/icons/tab-map-white.png";
+
+// profile icons
+import iconSmileyDarkPurple from "../assets/icons/icon-smiley-dark-purple.png";
+import iconSmileyLightPurple from "../assets/icons/icon-smiley-light-purple.png";
+import iconSmileyWhite from "../assets/icons/icon-smiley-white.png";
+import tabProfileDarkPurple from "../assets/icons/tab-profile-dark-purple.png";
+import tabProfileWhite from "../assets/icons/tab-profile-white.png";
