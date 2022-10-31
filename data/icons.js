956b3fb69544db14ba1d4b756d1aff1dd16ca276
William Sawyer <wmsawyer2609@gmail.com>
2022-10-31 11:44:44 +1000

    Reordered icons

--- data/icons.js
+++ data/icons.js
@@ -1,19 +1,21 @@
-// OgCiSum icons
+// meta application icons
 import appIcon from "../assets/icons/app-icon.png";
 import appLaunch from "../assets/icons/app-launch.png";
+
+// now playing pin icons
+import pinDarkPurple from "../assets/icons/icon-pin-dark-purple.png";
+import pinLightPurple from "../assets/icons/icon-pin-light-purple.png";
+import pinWhite from "../assets/icons/icon-pin-white.png";
+
+// now playing smiley icons
+import smileyDarkPurple from "../assets/icons/icon-smiley-dark-purple.png";
+import smileyLightPurple from "../assets/icons/icon-smiley-light-purple.png";
+import smileyWhite from "../assets/icons/icon-smiley-white.png";
+
+// bottom tab icons
 import logoDarkPurple from "../assets/icons/logo-dark-purple.png";
 import logoWhite from "../assets/icons/logo-white.png";
-
-// map icons
-import iconPinDarkPurple from "../assets/icons/icon-pin-dark-purple.png";
-import iconPinLightPurple from "../assets/icons/icon-pin-light-purple.png";
-import iconPinWhite from "../assets/icons/icon-pin-white.png";
 import tabMapDarkPurple from "../assets/icons/tab-map-dark-purple.png";
 import tabMapWhite from "../assets/icons/tab-map-white.png";
-
-// profile icons
-import iconSmileyDarkPurple from "../assets/icons/icon-smiley-dark-purple.png";
-import iconSmileyLightPurple from "../assets/icons/icon-smiley-light-purple.png";
-import iconSmileyWhite from "../assets/icons/icon-smiley-white.png";
 import tabProfileDarkPurple from "../assets/icons/tab-profile-dark-purple.png";
 import tabProfileWhite from "../assets/icons/tab-profile-white.png";
