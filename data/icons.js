b97d5f4e06d3189137e8f20720ac7e58aedd30dc
William Sawyer <wmsawyer2609@gmail.com>
2022-10-31 12:12:49 +1000

    Corrected icon filenames

--- data/icons.js
+++ data/icons.js
@@ -1,23 +1,23 @@
 // meta application icons
 import appIcon from "../assets/icons/app-icon.png";
-import appLaunch from "../assets/icons/app-launch.png";
+import appLaunch from "../assets/icons/app-launch.jpg";
 
 // now playing pin icons
-import pinDarkPurple from "../assets/icons/icon-pin-dark-purple.png";
-import pinLightPurple from "../assets/icons/icon-pin-light-purple.png";
+import pinDarkPurple from "../assets/icons/icon-pin-darkpurple.png";
+import pinLightPurple from "../assets/icons/icon-pin-lightpurple.png";
 import pinWhite from "../assets/icons/icon-pin-white.png";
 
 // now playing smiley icons
-import smileyDarkPurple from "../assets/icons/icon-smiley-dark-purple.png";
-import smileyLightPurple from "../assets/icons/icon-smiley-light-purple.png";
+import smileyDarkPurple from "../assets/icons/icon-smiley-darkpurple.png";
+import smileyLightPurple from "../assets/icons/icon-smiley-lightpurple.png";
 import smileyWhite from "../assets/icons/icon-smiley-white.png";
 
 // bottom tab icons
-import logoDarkPurple from "../assets/icons/logo-dark-purple.png";
+import logoDarkPurple from "../assets/icons/logo-darkpurple.png";
 import logoWhite from "../assets/icons/logo-white.png";
-import tabMapDarkPurple from "../assets/icons/tab-map-dark-purple.png";
+import tabMapDarkPurple from "../assets/icons/tab-map-darkpurple.png";
 import tabMapWhite from "../assets/icons/tab-map-white.png";
-import tabProfileDarkPurple from "../assets/icons/tab-profile-dark-purple.png";
+import tabProfileDarkPurple from "../assets/icons/tab-profile-darkpurple.png";
 import tabProfileWhite from "../assets/icons/tab-profile-white.png";
 
 export default {
