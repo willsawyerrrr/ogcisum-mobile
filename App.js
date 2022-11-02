f27f6eece79ae7009498d0b512490a5d6dee12ff
William Sawyer <wmsawyer2609@gmail.com>
2022-11-02 14:03:43 +1000

    Converted API data to be stateful

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView, useColorScheme } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
@@ -7,10 +7,33 @@ import BottomTabs from "./components/BottomTabs";
 import { readLocations, readSamples, readSamplesToLocations } from "./api/api";
 import { colours } from "./data/theme";
 
-export default async function App() {
-    const locations = await readLocations();
-    const samples = await readSamples();
-    const samplesToLocations = await readSamplesToLocations();
+export default function App() {
+    /** "Global" list of samples. */
+    const [samples, setSamples] = useState([]);
+
+    /** "Global" list of locations. */
+    const [locations, setLocations] = useState([]);
+
+    /** "Global" list of samples shared to locations. */
+    const [samplesToLocations, setSamplesToLocations] = useState([]);
+
+    /** Fetches samples from the WMP API. */
+    useEffect(() => {
+        const fetchSamples = async () => setSamples(await readSamples());
+        fetchSamples();
+    }, []);
+
+    /** Fetches locations from the WMP API. */
+    useEffect(() => {
+        const fetchLocations = async () => setLocations(await readLocations());
+        fetchLocations();
+    }, []);
+
+    /** Fetches samples shared to locations from the WMP API. */
+    useEffect(() => {
+        const fetchSamplesToLocations = async () => setSamplesToLocations(await readSamplesToLocations());
+        fetchSamplesToLocations();
+    }, []);
 
     const backgroundColour = colours[useColorScheme()].bgColour;
 
