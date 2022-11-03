c0e04fb5035ec9dba4847d0e840f10670c2be8c9
William Sawyer <wmsawyer2609@gmail.com>
2022-11-03 13:25:33 +1000

    Refactored shape of nearby location

--- helpers/calculateDistance.js
+++ helpers/calculateDistance.js
@@ -3,11 +3,10 @@ import getDistance from "geolib/es/getPreciseDistance";
 // Function to retrieve location nearest to current user location
 export default function calculateDistance(mapState, userLocation) {
     const nearestLocations = mapState.locations.map(location => {
-        const metres = getDistance(userLocation, location.coordinates);
-        location["distance"] = { metres: metres, nearby: metres <= NEARBY };
-        return location;
+        const distance = getDistance(userLocation, location.coordinates);
+        return { ...location, distance: distance, nearby: distance <= NEARBY };
     }).sort((previous, current) => {
-        return previous.distance.metres - current.distance.metres;
+        return previous.distance - current.distance;
     });
     return nearestLocations.shift();
 }
