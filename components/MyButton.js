e8d5ba0c03f04df342ae076b32d711e62d6aa192
William Sawyer <wmsawyer2609@gmail.com>
2022-11-04 18:02:33 +1000

    Utilised style props

--- components/MyButton.js
+++ components/MyButton.js
@@ -2,11 +2,14 @@ import { Text, TouchableOpacity, useColorScheme } from "react-native";
 
 import { darkStyles, lightStyles } from "../data/theme";
 
-export default function MyButton({ onPress, text }) {
+export default function MyButton({ onPress, style, text }) {
     const styles = (useColorScheme() === "dark") ? darkStyles : lightStyles;
 
     return (
-        <TouchableOpacity onPress={onPress} style={styles.button}>
+        <TouchableOpacity
+            onPress={onPress}
+            style={{ ...styles.button, ...style }}
+        >
             <Text style={styles.buttonText}>
                 {text}
             </Text>
